7c2478a8cb9e1fc8bf3aa2f35832951093f00543
Abir-Ghosh16 <abir20021116@gmail.com>
2026-03-18 22:43:09 +0600

    chore: remove build artifacts and IDE files, add .gitignore

--- LeaveCalculation/src/Staff.java
+++
@@ -1,9 +0,0 @@
-public class Staff extends Employee
-{
-    int vacationLeave,sickLeave;
-
-    public Staff(int id, String name, String dateOfBirth, String email, String joiningDate, String type)
-    {
-        super(id, name, dateOfBirth, email, joiningDate,type);
-    }
-}
